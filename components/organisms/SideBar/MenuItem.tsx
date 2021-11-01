a58f7f3c3078c1458701921ca5e19b7336c8cc25
shafanaura <shafanaura48@gmail.com>
2021-11-01 19:19:04 +0700

    Refactor Edit Profile

--- components/organisms/SideBar/MenuItem.tsx
+++ components/organisms/SideBar/MenuItem.tsx
@@ -1,14 +1,16 @@
 import cx from 'classnames';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface MenuItemProps {
   title: string;
   icon: string;
   active?: boolean;
+  href: string;
 }
 
 const MenuItem = (props: Partial<MenuItemProps>) => {
-  const { title, icon, active } = props;
+  const { title, icon, active, href } = props;
   const classItem = cx({ item: true, 'mb-30': true, active });
   return (
     <div className={classItem}>
@@ -16,9 +18,9 @@ const MenuItem = (props: Partial<MenuItemProps>) => {
         <Image src={`/icon/ic-menu-${icon}.svg`} width={25} height={25} />
       </div>
       <p className="item-title m-0">
-        <a href="" className="text-lg text-decoration-none">
-          {title}
-        </a>
+        <Link href={href}>
+          <a className="text-lg text-decoration-none">{title}</a>
+        </Link>
       </p>
     </div>
   );
--- components/organisms/SideBar/index.tsx
+++ components/organisms/SideBar/index.tsx
@@ -2,22 +2,44 @@ import Footer from './Footer';
 import Profile from './Profile';
 import MenuItem from './MenuItem';
 
-const SideBar = () => (
-  <section className="sidebar">
-    <div className="content pt-50 pb-30 ps-30">
-      <Profile />
-      <div className="menus">
-        <MenuItem title="Overview" icon="overview" active />
-        <MenuItem title="Transactions" icon="transaction" />
-        <MenuItem title="Messages" icon="message" />
-        <MenuItem title="Card" icon="card" />
-        <MenuItem title="Rewards" icon="reward" />
-        <MenuItem title="Settings" icon="setting" />
-        <MenuItem title="Log Out" icon="logout" />
+interface SideBarProps {
+  activeMenu: 'overview' | 'transactions' | 'settings';
+}
+
+const SideBar = (props: SideBarProps) => {
+  const { activeMenu } = props;
+  return (
+    <section className="sidebar">
+      <div className="content pt-50 pb-30 ps-30">
+        <Profile />
+        <div className="menus">
+          <MenuItem
+            title="Overview"
+            icon="overview"
+            active={activeMenu === 'overview'}
+            href="/member"
+          />
+          <MenuItem
+            active={activeMenu === 'transactions'}
+            title="Transactions"
+            icon="transaction"
+            href="/member/transactions"
+          />
+          <MenuItem title="Messages" icon="message" href="/member" />
+          <MenuItem title="Card" icon="card" href="/member" />
+          <MenuItem title="Rewards" icon="reward" href="/member/" />
+          <MenuItem
+            active={activeMenu === 'settings'}
+            title="Settings"
+            icon="setting"
+            href="/member/edit-profile"
+          />
+          <MenuItem title="Log Out" icon="logout" href="/sign-in" />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default SideBar;
